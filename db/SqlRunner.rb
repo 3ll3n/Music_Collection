50f6f9aa0e4302df51a45b734583aacfab1cfd92
3ll3n <ellengrafton01@gmail.com>
2017-01-25 14:29:36 +0000

    Added ; to music_collection.sql and updated SqlRunner.rb

---
+++ db/SqlRunner.rb
@@ -0,0 +1,14 @@
+require_relative('pg')
+
+class SqlRunner()
+
+  def self.run(sql)
+   begin
+      db = PG.connect({dbname: 'Music', :host 'localhost'})
+      result = db.exec(sql)
+    ensure
+      db.close
+    end
+  return result
+  end
+end
